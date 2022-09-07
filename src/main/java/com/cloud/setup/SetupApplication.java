e0755f3cbe3ad3a1cd3339561cfd2e7f44ba5217
Obinna Ogbonna <obinnaogbonnajoseph@gmail.com>
2022-09-07 12:43:56 +0100

    server-side encryption and decryption

--- src/main/java/com/cloud/setup/SetupApplication.java
+++ src/main/java/com/cloud/setup/SetupApplication.java
@@ -2,9 +2,7 @@ package com.cloud.setup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
 public class SetupApplication {
 
